953f1402169561acb9cc0e198fe3f0d552fc8ce4
meenakshiravisankar <ravisankarmeenakshi@gmail.com>
2020-04-08 08:41:25 +0530

    added support for interlaced

--- network/__init__.py
+++ network/__init__.py
@@ -2,13 +2,14 @@ from .resnet101_baseline import get_resnet101_baseline
 from .resnet101_base_oc import get_resnet101_base_oc_dsn
 from .resnet101_pyramid_oc import get_resnet101_pyramid_oc_dsn
 from .resnet101_asp_oc import get_resnet101_asp_oc_dsn
-
+from .resnet101_interlaced import get_resnet101_interlaced_dsn
 
 networks = {
     'resnet101_baseline': get_resnet101_baseline,
     'resnet101_base_oc_dsn': get_resnet101_base_oc_dsn,
     'resnet101_pyramid_oc_dsn': get_resnet101_pyramid_oc_dsn,
     'resnet101_asp_oc_dsn': get_resnet101_asp_oc_dsn,
+    'resnet101_interlaced' : get_resnet101_interlaced_dsn
 }
 
 def get_segmentation_model(name, **kwargs):
