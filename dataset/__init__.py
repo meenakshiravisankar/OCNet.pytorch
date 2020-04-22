b8daccfa330f3a69d3f16730626d4106bc7b3f09
meenakshiravisankar <ravisankarmeenakshi@gmail.com>
2020-04-22 09:31:07 +0530

    added support for idd

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,12 +1,18 @@
 from .cityscapes import CitySegmentationTrain, CitySegmentationTest, CitySegmentationTrainWpath
 from .ade20k import Ade20kSegmentationTrain, Ade20kSegmentationTest, Ade20kSegmentationTrainWpath
+from .idd import IddSegmentationTrain, IddSegmentationTest, IddSegmentationTrainWpath
+
+
 datasets = {
 	'cityscapes_train': CitySegmentationTrain,
 	'cityscapes_test': CitySegmentationTest,
 	'cityscapes_train_w_path': CitySegmentationTrainWpath,
     'ade20k_train' : Ade20kSegmentationTrain,
     'ade20k_test' : Ade20kSegmentationTest,
-    'ade20k_train_w_path' : Ade20kSegmentationTrainWpath
+    'ade20k_train_w_path' : Ade20kSegmentationTrainWpath,
+    'idd_train' : IddSegmentationTrain,
+    'idd_test' : IddSegmentationTest,
+    'idd_train_w_path' : IddSegmentationTrainWpath
 }
 
 
