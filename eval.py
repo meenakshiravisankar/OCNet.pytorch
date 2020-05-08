e3f071604f86cf6e8a0449f7ab2c6eaa8d8eb8f5
meenakshiravisankar <ravisankarmeenakshi@gmail.com>
2020-05-08 23:23:08 +0530

    added code for hierarchical mIOU

--- eval.py
+++ eval.py
@@ -37,6 +37,8 @@ import mlflow
 import matplotlib.pyplot as plt
 import torch.nn as nn
 
+from collections import namedtuple
+
 torch_ver = torch.__version__[:3]
 
 # TODO : change to idd
@@ -301,7 +303,74 @@ def main():
 
     data_list = []
     confusion_matrix = np.zeros((args.num_classes,args.num_classes))
+    # level 2
+    confusion_matrix_2 = np.zeros((16, 16))
+    # level 1
+    confusion_matrix_1 = np.zeros((7, 7))
+    
+    Label = namedtuple( 'Label' , [
 
+    'name'        , 
+    'id'          ,
+
+    'csId'        ,
+
+    'csTrainId'   ,    
+
+    'level4Id'    , 
+    'level3Id'    , 
+    'level2IdName', 
+    'level2Id'    , 
+    'level1Id'    , 
+
+    'hasInstances', 
+    'ignoreInEval', 
+    'color'       , 
+    ] )
+    labels = [
+    #       name                     id    csId     csTrainId level4id        level3Id  category           level2Id      level1Id  hasInstances   ignoreInEval   color
+    Label(  'road'                 ,  0   ,  7 ,     0 ,       0   ,     0  ,   'drivable'            , 0           , 0      , False        , False        , (128, 64,128)  ),
+    Label(  'parking'              ,  1   ,  9 ,   255 ,       1   ,     1  ,   'drivable'            , 1           , 0      , False        , False         , (250,170,160)  ),
+    Label(  'drivable fallback'    ,  2   ,  255 ,   255 ,     2   ,       1  ,   'drivable'            , 1           , 0      , False        , False         , ( 81,  0, 81)  ),
+    Label(  'sidewalk'             ,  3   ,  8 ,     1 ,       3   ,     2  ,   'non-drivable'        , 2           , 1      , False        , False        , (244, 35,232)  ),
+    Label(  'rail track'           ,  4   , 10 ,   255 ,       3   ,     3  ,   'non-drivable'        , 3           , 1      , False        , False         , (230,150,140)  ),
+    Label(  'non-drivable fallback',  5   , 255 ,     9 ,      4   ,      3  ,   'non-drivable'        , 3           , 1      , False        , False        , (152,251,152)  ),
+    Label(  'person'               ,  6   , 24 ,    11 ,       5   ,     4  ,   'living-thing'        , 4           , 2      , True         , False        , (220, 20, 60)  ),
+    Label(  'animal'               ,  7   , 255 ,   255 ,      6   ,      4  ,   'living-thing'        , 4           , 2      , True         , True        , (246, 198, 145)),
+    Label(  'rider'                ,  8   , 25 ,    12 ,       7   ,     5  ,   'living-thing'        , 5           , 2      , True         , False        , (255,  0,  0)  ),
+    Label(  'motorcycle'           ,  9   , 32 ,    17 ,       8   ,     6  ,   '2-wheeler'           , 6           , 3      , True         , False        , (  0,  0,250)  ),
+    Label(  'bicycle'              , 10   , 33 ,    18 ,       9   ,     7  ,   '2-wheeler'           , 6           , 3      , True         , False        , (119, 11, 32)  ),
+    Label(  'autorickshaw'         , 11   , 255 ,   255 ,     10   ,      8  ,   'autorickshaw'        , 7           , 3      , True         , False        , (255, 204, 54) ),
+    Label(  'car'                  , 12   , 26 ,    13 ,      11   ,     9  ,   'car'                 , 7           , 3      , True         , False        , (  0,  0,120)  ),
+    Label(  'truck'                , 13   , 27 ,    14 ,      12   ,     10 ,   'large-vehicle'       , 8           , 3      , True         , False        , (  0,  0, 70)  ),
+    Label(  'bus'                  , 14   , 28 ,    15 ,      13   ,     11 ,   'large-vehicle'       , 8           , 3      , True         , False        , (  0, 60,100)  ),
+    Label(  'caravan'              , 15   , 29 ,   255 ,      14   ,     12 ,   'large-vehicle'       , 8           , 3      , True         , True         , (  0,  0, 90)  ),
+    Label(  'trailer'              , 16   , 30 ,   255 ,      15   ,     12 ,   'large-vehicle'       , 8           , 3      , True         , True         , (  0,  0,110)  ),
+    Label(  'train'                , 17   , 31 ,    16 ,      15   ,     12 ,   'large-vehicle'       , 8           , 3      , True         , True        , (  0, 80,100)  ),
+    Label(  'vehicle fallback'     , 18   , 355 ,   255 ,     15   ,      12 ,   'large-vehicle'       , 8           , 3      , True         , False        , (136, 143, 153)),  
+    Label(  'curb'                 , 19   ,255 ,   255 ,      16   ,     13 ,   'barrier'             , 9           , 4      , False        , False        , (220, 190, 40)),
+    Label(  'wall'                 , 20   , 12 ,     3 ,      17   ,     14 ,   'barrier'             , 9           , 4      , False        , False        , (102,102,156)  ),
+    Label(  'fence'                , 21   , 13 ,     4 ,      18   ,     15 ,   'barrier'             , 10           , 4      , False        , False        , (190,153,153)  ),
+    Label(  'guard rail'           , 22   , 14 ,   255 ,      19   ,     16 ,   'barrier'             , 10          , 4      , False        , False         , (180,165,180)  ),
+    Label(  'billboard'            , 23   , 255 ,   255 ,     20   ,      17 ,   'structures'          , 11           , 4      , False        , False        , (174, 64, 67) ),
+    Label(  'traffic sign'         , 24   , 20 ,     7 ,      21   ,     18 ,   'structures'          , 11          , 4      , False        , False        , (220,220,  0)  ),
+    Label(  'traffic light'        , 25   , 19 ,     6 ,      22   ,     19 ,   'structures'          , 11          , 4      , False        , False        , (250,170, 30)  ),
+    Label(  'pole'                 , 26   , 17 ,     5 ,      23   ,     20 ,   'structures'          , 12          , 4      , False        , False        , (153,153,153)  ),
+    Label(  'polegroup'            , 27   , 18 ,   255 ,      23   ,     20 ,   'structures'          , 12          , 4      , False        , False         , (153,153,153)  ),
+    Label(  'obs-str-bar-fallback' , 28   , 255 ,   255 ,     24   ,      21 ,   'structures'          , 12          , 4      , False        , False        , (169, 187, 214) ),  
+    Label(  'building'             , 29   , 11 ,     2 ,      25   ,     22 ,   'construction'        , 13          , 5      , False        , False        , ( 70, 70, 70)  ),
+    Label(  'bridge'               , 30   , 15 ,   255 ,      26   ,     23 ,   'construction'        , 13          , 5      , False        , False         , (150,100,100)  ),
+    Label(  'tunnel'               , 31   , 16 ,   255 ,      26   ,     23 ,   'construction'        , 13          , 5      , False        , False         , (150,120, 90)  ),
+    Label(  'vegetation'           , 32   , 21 ,     8 ,      27   ,     24 ,   'vegetation'          , 14          , 5      , False        , False        , (107,142, 35)  ),
+    Label(  'sky'                  , 33   , 23 ,    10 ,      28   ,     25 ,   'sky'                 , 15          , 6      , False        , False        , ( 70,130,180)  ),
+    Label(  'fallback background'  , 34   , 255 ,   255 ,     29   ,      25 ,   'object fallback'     , 15          , 6      , False        , False        , (169, 187, 214)),
+    Label(  'unlabeled'            , 35   ,  0  ,     255 ,   255   ,      255 ,   'void'                , 255         , 255    , False        , True         , (  0,  0,  0)  ),
+    Label(  'ego vehicle'          , 36   ,  1  ,     255 ,   255   ,      255 ,   'void'                , 255         , 255    , False        , True         , (  0,  0,  0)  ),
+    Label(  'rectification border' , 37   ,  2  ,     255 ,   255   ,      255 ,   'void'                , 255         , 255    , False        , True         , (  0,  0,  0)  ),
+    Label(  'out of roi'           , 38   ,  3  ,     255 ,   255   ,      255 ,   'void'                , 255         , 255    , False        , True         , (  0,  0,  0)  ),
+    Label(  'license plate'        , 39   , 255 ,     255 ,   255   ,      255 ,   'vehicle'             , 255         , 255    , False        , True         , (  0,  0,142)  ),
+    
+]   
     palette = get_palette(20)
 
     image_id = 0
@@ -343,11 +412,33 @@ def main():
                         else:
                             output = predict_whole_img(model, image.numpy(), args.num_classes, 
                                 args.method, scale=float(args.whole_scale))
-
+        # level 3 predictions
         seg_pred = np.asarray(np.argmax(output, axis=3), dtype=np.uint8)
         m_seg_pred = ma.masked_array(seg_pred, mask=torch.eq(label, 26)) # dataset specific
         ma.set_fill_value(m_seg_pred, 26) # dataset specific
         seg_pred = m_seg_pred
+
+        # level 2 predictions
+        seg_pred_2 = np.array(seg_pred)
+        for class_label in labels:
+            label_id_src = class_label.level3Id
+            label_id_dst = class_label.level2Id
+            seg_pred_2[seg_pred_2==label_id_src] = label_id_dst
+        
+        m_seg_pred_2 = ma.masked_array(seg_pred_2, mask=torch.eq(label, 26)) # dataset specific
+        ma.set_fill_value(m_seg_pred_2, 26) # dataset specific
+        seg_pred_2 = m_seg_pred_2
+
+        # level 1 predictions
+        seg_pred_1 = np.array(seg_pred)
+        for class_label in labels:
+            label_id_src = class_label.level3Id
+            label_id_dst = class_label.level1Id
+            seg_pred_1[seg_pred_1==label_id_src] = label_id_dst
+        
+        m_seg_pred_1 = ma.masked_array(seg_pred_1, mask=torch.eq(label, 26)) # dataset specific
+        ma.set_fill_value(m_seg_pred_1, 26) # dataset specific
+        seg_pred_1 = m_seg_pred_1
 
         for i in range(image.size(0)): 
             image_id += 1
@@ -364,8 +455,14 @@ def main():
         ignore_index = seg_gt != 26 # dataset specific
         seg_gt = seg_gt[ignore_index]
         seg_pred = seg_pred[ignore_index]
+        seg_pred_2 = seg_pred_2[ignore_index]
+        seg_pred_1 = seg_pred_1[ignore_index]
+        
+        print(seg_pred.shape)
         confusion_matrix += get_confusion_matrix(seg_gt, seg_pred, args.num_classes)
-            
+        confusion_matrix_2 += get_confusion_matrix(seg_gt, seg_pred_2, 16)
+        confusion_matrix_1 += get_confusion_matrix(seg_gt, seg_pred_1, 7)
+    # level 3
     pos = confusion_matrix.sum(1)
     res = confusion_matrix.sum(0)
     tp = np.diag(confusion_matrix)
@@ -380,6 +477,45 @@ def main():
     mlflow.log_param("mean_iu", mean_IU)
     mlflow.log_param("iu_array", IU_array)
     mlflow.log_param("confusion_matrix", confusion_matrix)
+
+    # level 2
+    pos = confusion_matrix_2.sum(1)
+    res = confusion_matrix_2.sum(0)
+    tp = np.diag(confusion_matrix_2)
+
+    IU_array_2 = (tp / np.maximum(1.0, pos + res - tp))
+    mean_IU_2 = IU_array_2.mean()
+    
+    print({'meanIU':mean_IU_2, 'IU_array_2':IU_array_2})
+
+    print("confusion matrix\n")
+    print(confusion_matrix_2)
+    mlflow.log_param("mean_iu_2", mean_IU_2)
+    mlflow.log_param("iu_array_2", IU_array_2)
+    mlflow.log_param("confusion_matrix_2", confusion_matrix_2)
+
+    # level 1
+    pos = confusion_matrix_1.sum(1)
+    res = confusion_matrix_1.sum(0)
+    tp = np.diag(confusion_matrix_1)
+
+    IU_array_1 = (tp / np.maximum(1.0, pos + res - tp))
+    mean_IU_1 = IU_array_1.mean()
+    
+    print({'meanIU':mean_IU_1, 'IU_array_1':IU_array_1})
+
+    print("confusion matrix\n")
+    print(confusion_matrix_1)
+    print("#######")
+    print("Summary")
+    print("#######")
+    print("Level 3 mIOU = ", mean_IU)
+    print("Level 2 mIOU = ", mean_IU_2)
+    print("Level 1 mIOU = ", mean_IU_1)
+    
+    mlflow.log_param("mean_iu_1", mean_IU_1)
+    mlflow.log_param("iu_array_1", IU_array_1)
+    mlflow.log_param("confusion_matrix_1", confusion_matrix_1)
     sys.stdout.flush()
 
 if __name__ == '__main__':
