687077cd871ea5fb9dfc240e492a4db96c6a46d4
meenakshiravisankar <ravisankarmeenakshi@gmail.com>
2020-05-15 11:50:18 +0530

    added code for fine-tune

--- config/__init__.py
+++ config/__init__.py
@@ -97,7 +97,9 @@ class Parameters():
         parser.add_argument("--pair-weight", type=float, default=1,
                             help="choose the weight of the pair-wise loss supervision.")
         parser.add_argument('--seed', default=304, type=int, help='manual seed')
-        parser.add_argument("--finetune", action="store_true",
+        parser.add_argument("--fine-tune", action="store_true",
+                            help="Whether to finetune.")
+        parser.add_argument("--resume-training", action="store_true",
                             help="Whether to finetune.")
 
         parser.add_argument("--output-path", type=str, default='./seg_output_eval_set',
--- run_idd_train.sh
+++ run_idd_train.sh
@@ -6,15 +6,15 @@ PYTHON="python3"
 
 #network config
 NETWORK="resnet101"
-METHOD="base_oc_dsn"
+METHOD="interlaced_dsn"
 DATASET="idd_train"
 NUM_CLASSES=26
 
 #training settings
 LEARNING_RATE=5e-3
 WEIGHT_DECAY=5e-4
-START_ITERS=16000
-MAX_ITERS=40000
+START_ITERS=80000
+MAX_ITERS=120000
 BATCHSIZE=4
 INPUT_SIZE='769,769'
 USE_CLASS_BALANCE=True
@@ -29,8 +29,8 @@ EXPERIMENT_NAME="$1"
 
 # replace the DATA_DIR with your folder path to the dataset.
 DATA_DIR='./dataset/idd'
-DATA_LIST_PATH='./dataset/list/idd/train_1.lst'
-RESTORE_FROM='checkpoint/snapshots_resnet101_base_oc_dsn_5e-3_5e-4_4_40000/CS_scenes_16000.pth'
+DATA_LIST_PATH='./dataset/list/idd/train_2.lst'
+RESTORE_FROM='mlruns/3/train_52_80/artifacts/CS_scenes_80000.pth'
 
 # Set the Output path of checkpoints, training log.
 TRAIN_LOG_DIR="./log/log_train"
@@ -44,7 +44,7 @@ mkdir -p ${CHECKPOINT_DIR}
 ########################################################################################################################
 #  Training
 ########################################################################################################################
-$PYTHON -u train.py --num-classes $NUM_CLASSES --experiment-name $EXPERIMENT_NAME --network $NETWORK --method $METHOD --random-mirror --random-scale --gpu 0,1,2,3 --batch-size $BATCHSIZE \
+$PYTHON -u train.py --fine_tune --num-classes $NUM_CLASSES --experiment-name $EXPERIMENT_NAME --network $NETWORK --method $METHOD --random-mirror --random-scale --gpu 0,1,2,3 --batch-size $BATCHSIZE \
   --snapshot-dir $SNAPSHOT_DIR  --num-steps $MAX_ITERS --ohem $USE_OHEM --data-list $DATA_LIST_PATH --weight-decay $WEIGHT_DECAY \
   --input-size $INPUT_SIZE --ohem-thres $OHEMTHRES --ohem-keep $OHEMKEEP --use-val $USE_VAL_SET --use-weight $USE_CLASS_BALANCE \
   --snapshot-dir $SNAPSHOT_DIR --restore-from $RESTORE_FROM --start-iters $START_ITERS --learning-rate $LEARNING_RATE  \
